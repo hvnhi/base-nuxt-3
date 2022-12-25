ad1bb27a1d295474070a61eeea33ab6d1b9c1a1e
nhihv <nhi.hv@vn-cubesystem.com>
2022-12-25 22:57:39 +0700

    config prettier, piana, tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    // pinia plugin https://nuxt.com/modules/pinia
+    '@pinia/nuxt',
+    // tailwindcss https://tailwindcss.nuxt.dev/getting-started/setup/
+    '@nuxtjs/tailwindcss',
+  ],
 })
